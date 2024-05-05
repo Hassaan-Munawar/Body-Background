e3831f934112929bce6d39a5105d4fba9f0a128d
Hassaan-Munawar <mhassaanmunawarali@gmail.com>
2024-05-05 18:58:38 +0500

    code

---
+++ app.js
@@ -0,0 +1,18 @@
+function red(){
+    document.getElementById("body").style.backgroundColor ="red"
+}
+function blue(){
+    document.getElementById("body").style.backgroundColor ="blue"
+}
+function green(){
+    document.getElementById("body").style.backgroundColor ="green"
+}
+function aqua(){
+    document.getElementById("body").style.backgroundColor ="aqua"
+}
+function white(){
+    document.getElementById("body").style.backgroundColor ="white"
+}
+function black(){
+    document.getElementById("body").style.backgroundColor ="black"
+}
